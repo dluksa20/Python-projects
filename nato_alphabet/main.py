06e0ad9ca04c62cf18ea1bd6f93dff8a05294c37
dluksa20 <dluksa20@gmail.com>
2025-10-13 00:03:05 +0100

    invalid symbol exception handling

--- nato_alphabet/main.py
+++ nato_alphabet/main.py
@@ -1,21 +1,20 @@
 import pandas
 
 df = pandas.read_csv('./nato_phonetic_alphabet.csv')
-user_name = input('Enter your name: ').upper()
-
-
-#  SOLUTION 1
-code = []
-
-for letter in user_name:
-    for (index, row) in df.iterrows():
-        if letter == row.letter:
-            code.append(row.code)
-
-
-print(code)
-
-# SOLUTION 2
 code = {row.letter:row.code for (index, row) in df.iterrows()}
-answer = [code[letter] for letter in user_name]
-print(answer)
+
+
+# SOLUTION 
+
+def generate_phonetic(code):
+    user_name = input('Enter your name: ').upper()
+
+    try:
+        answer = [code[letter] for letter in user_name]
+    except KeyError:
+        print('Sorry, only letters in the alphabet please.')
+    else:
+        print(answer)
+
+if __name__ == '__main__':
+    generate_phonetic(code)
