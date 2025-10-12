546889929b5d6eb952f4d6f3ef6cff5b5e888c24
dluksa20 <dluksa20@gmail.com>
2025-10-12 17:54:41 +0100

    finished

--- password_manager_gui/main.py
+++ password_manager_gui/main.py
@@ -1,50 +1,113 @@
 import tkinter
 import customtkinter
+from tkinter import messagebox
+
+
+
+
+
+
+import string
+import random
+
+
+# ----------------------------- GENERATE PASSWORD -------------------------------- #
+def generate_password():
+
+
+    password_list = []
+    # 
+    letters = string.ascii_letters
+    numbers_list = string.digits
+    special_symbols = string.punctuation[0:17]
+    # 
+    password_letters = [random.choice(letters) for _ in range(random.randint(8,10))]
+    password_numbers = [random.choice(numbers_list) for _ in range(random.randint(8,10))]
+    password_symbols = [random.choice(special_symbols) for _ in range(random.randint(2,4))]
+    # 
+    
+    password_list = password_letters + password_numbers + password_symbols
+    random.shuffle(password_list)
+
+    password = ''.join(password_list)
+    if len(password_input.get()) != 0 :
+        password_input.delete(0, tkinter.END)
+    password_input.insert(0, password)
+
+    
+
+
+# ----------------------------- SAVE DATA -------------------------------- #
+def save_data():
+
+    # get entries
+    website = url_input.get()
+    user = username_input.get()
+    pasword = password_input.get()
+
+
+    # show warning for empty fields
+    if len(user) == 0 or len(website) == 0 or len(pasword) == 0:
+        is_empty = messagebox.showwarning(title='Warning', message='Invalid input, fields can not be empty!')
+    else:
+        is_ok = messagebox.askokcancel(title=website, message=f'These are the details entered:' 
+ 
+                                                  f'\nEmail: {user} \nPassword: {pasword} \n Is it ok to save?')
+        if is_ok:
+            with open('data.txt', 'a') as f:
+                f.write(f'{website} | {user} | {pasword} \n')
+                # clear the entries
+                url_input.delete(0, tkinter.END)
+                password_input.delete(0, tkinter.END)
+                f.close()
+
 
 
 # ----------------------------- NEW WINDOW -------------------------------- #
 window = tkinter.Tk()
 window.title('Password Manager')
-window.config(padx=20, pady=20)
+window.config(padx=50, pady=50)
 
 
 # ----------------------------- LOGO -------------------------------- #
 canvas = tkinter.Canvas(width=200, height=200)
 lock_image = tkinter.PhotoImage(file='logo.png')
 canvas.create_image(100, 100, image=lock_image)
-canvas.grid(row=0, column=1)
+canvas.grid(row=0, column=0, columnspan=3)
 
 
 # ----------------------------- URL LABEL/INPUT -------------------------------- #
 url_label = tkinter.Label(text='Website: ')
-url_label.grid(row=1, column=0)
-url_input = tkinter.Entry(width=35)
-url_input.grid(row=1, column=1, columnspan=2, sticky='EW')
+url_label.grid(row=1, column=0, sticky='E')
+url_input = tkinter.Entry(width=45)
+url_input.grid(row=1, column=1, columnspan=2)
+url_input.focus()
 
 
 
 # ----------------------------- USERNAME LABEL/INPUT -------------------------------- #
 username_label = tkinter.Label(text='Username: ')
-username_label.grid(row=2, column=0)
+username_label.grid(row=2, column=0, sticky='E')
 
-username_input = tkinter.Entry(width=35)
-username_input.grid(row=2, column=1, columnspan=2, sticky='EW')
-2
+username_input = tkinter.Entry(width=45)
+username_input.grid(row=2, column=1, columnspan=2, pady=(3, 0))
+username_input.insert(0, 'name@gmail.com')
+
 
 # ----------------------------- PASSWORD LABEL/INPUT/BUTTON -------------------------------- #
 password_label = tkinter.Label(text='Password: ')
-password_label.grid(row=3, column=0)
+password_label.grid(row=3, column=0, sticky='E')
 
-password_input = tkinter.Entry(width=21)
-password_input.grid(row=3, column=1, sticky='W')
+password_input = tkinter.Entry(width=25)
+password_input.grid(row=3, column=1, sticky='E', pady=(3, 0))
 
-generate_button = tkinter.Button(text='Generate Password')
-generate_button.grid(row=3, column=2)
+generate_button = tkinter.Button(text='Generate Password',width=15, command=generate_password)
+generate_button.grid(row=3, column=2, sticky='W', padx=(5,0), pady=(3, 0))
 
 
 # ----------------------------- ADD BUTTON -------------------------------- #
-add_button = tkinter.Button(text='Add', width=36)
-add_button.grid(row=4, column=1, columnspan=2, sticky='WE')
+add_button = tkinter.Button(text='Add', width=38, command=save_data)
+add_button.grid(row=4, column=1, columnspan=2, pady=(5, 0))
 
 
 
